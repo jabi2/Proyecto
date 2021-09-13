d2a41856ec9be180057314ac2e8a8007adca222d
Javier <87917509+jabi2@users.noreply.github.com>
2021-09-13 03:19:08 +0000

    Se agregaron encabezados y comentarios iniciales

--- Controlador.java
+++ Controlador.java
@@ -1,6 +1,14 @@
+/**********************
+Proyecto POO 
+Clase Controlador
+Autores: Javier Bocanegra, Lucy Elías, Juan Marroquín y Herber Silva.
+Descripcion: Controlador del programa
+Version: 1.0
+Ultima actualizacion: 12 de septiembre
+******************************************************************/
+
 class Controlador{
-  public static void main(String[] args)
-  {
+	
     
-  }
+  
 }
--- Vista.java
+++ Vista.java
@@ -1,8 +1,12 @@
 /**********************
 Proyecto POO 
-
-Descripcion: Vista del programa
+Clase Vista
+Autores: Javier Bocanegra, Lucy Elías, Juan Marroquín y Herber Silva.
+Descripcion: Vista del programa, permite al usuario interactuar con el programa y mostrar informacion relevante.
+Version: 1.0
+Ultima actualizacion: 12 de septiembre
 ******************************************************************/
+/*Se importa scanner para poder leer los datos ingresados por el usuario*/
 import java.util.Scanner;
 
 class Vista{
@@ -15,15 +19,15 @@ class Vista{
 
 
 		while (opcion != 4){
-
+/*Se crea el menú de opciones que se le presentará al usuario para que el mismo pueda escoger la opción deseada*/
 			System.out.println("\n\n Bienvenido al programa que busca apoyar al rendimiento y desarrollo de los estudiantes.");
 			System.out.println("1. Cursos\n");
 			System.out.println("2. Programas Universitarios\n");
 			System.out.println("3. Contactos para tutorías\n");
 			System.out.println("4. Salir\n\n");
-
+/*Se implementa un ciclo While el que leera el número de opción que desea ejecutar el usuario y asi ejecutarlo*/
 			opcion = scan.nextInt();
-
+/*cada condición permite el ingreso al método estipulado para cada opción*/
 			if (opcion == 1){
         //controlador.cursos();
       }
