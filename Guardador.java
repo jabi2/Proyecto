0c882a80914363da0221562498ddcb0d40ecf0ba
Javier <87917509+jabi2@users.noreply.github.com>
2021-10-11 03:31:58 +0000

    Se agrego la clase Guardador la cual se espera que substituya a la clase Libreta y permita el manejo de un archivo de texto o csv. Principalmente un place holder útil para la ideación.

---
+++ Guardador.java
@@ -0,0 +1,12 @@
+/**********************
+Proyecto POO 
+Clase Guardador
+Autores: Javier Bocanegra, Lucy Elías, Juan Marroquín y Herber Silva.
+Descripcion: Permite extraer los contactos de un archivo en donde estén guardados y así utilizarlos en el programa. 
+Version: 1.0
+Ultima actualizacion: 10 de octubre
+******************************************************************/
+
+/** 
+Placeholder por que se requiere investigar más acerca de la clase File y así importarla y utilizarla aca. Investigación en proceso.
+**/
