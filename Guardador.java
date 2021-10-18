6b89dbcb256285a6e375b6952d918a2efe09e0c6
Javier <87917509+jabi2@users.noreply.github.com>
2021-10-18 05:55:08 +0000

    Avance del lector de excel.

--- Guardador.java
+++ Guardador.java
@@ -10,3 +10,35 @@ Ultima actualizacion: 10 de octubre
 /** 
 Placeholder por que se requiere investigar más acerca de la clase File y así importarla y utilizarla aca. Investigación en proceso.
 **/
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+public class Guardador{
+
+	private String path = "";
+	private String line = "";
+
+  /********************************
+     * @param: -
+     * @return: -
+     */
+  //Es el constructor de la clase.
+	public Guardador()
+	{
+	}
+
+	try {
+		BufferedReader br = new BufferedReader(new FileReader(path));	
+
+		while((line = br.readLine()) != null){
+			System.out.println(line);
+			String[] values = line.split(",");
+			System.out.println(values[0]);
+		}
+
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+
