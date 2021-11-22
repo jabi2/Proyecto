c86b1e8aa3186875e3cd45431d2228f94f452e0e
Sebastián Silva <77755597+hsilv@users.noreply.github.com>
2021-11-22 03:52:00 +0000

    Se creó la interfaz Estimador, que posee un método llamado GetUniIndex que nos ayuda a recomendar universidades.

---
+++ Estimador.java
@@ -0,0 +1,18 @@
+/**********************
+Proyecto POO 
+Interfaz Estimador
+Autores: Javier Bocanegra, Lucy Elías, Juan Marroquín y Herber Silva.
+Descripcion: Interfaz que sirve para sacar promedios de puntaje de un cuestionario
+Version: 1.0
+Ultima actualizacion: 21 de noviembre de 2021
+******************************************************************/
+
+public interface Estimador {
+
+    /**
+     *
+     * @param c
+     * @return
+     */
+    public double getUniIndex(CuestionarioUni c);
+}
